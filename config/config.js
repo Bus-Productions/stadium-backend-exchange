9b7593e2fe2743e1e437f9de73439146c709ad7e
Nathan Loyer <4namlet@gmail.com>
2013-06-25 14:08:42 -0500

    add basic structure

---
+++ config/config.js
@@ -0,0 +1,43 @@
+var env = process.env.GAME_ENV || "development";
+
+console.log("Using the "+ env + " environment");
+
+exports.config = function(){
+  if (env == "development"){
+    return {
+      env: 'development',
+      dbname: 'stadium_exchange',
+      dbuser: 'stadium_exchange',
+      dbpass: 'game2013',
+      dbhost: 'localhost',
+      dbport: 5432,
+      host: 'localhost',
+      port: 4100,
+      method: 'POST'
+    };
+  } else if (env == "staging"){
+    return {
+      env: 'staging',
+      dbname: 'stadium_exchange',
+      dbuser: 'stadium_exchange',
+      dbpass: 'game2013',
+      dbhost: 'poop.compute-1.amazonaws.com',
+      dbport: 5432,
+      host: 'blah.elasticbeanstalk.com',
+      port: process.env.PORT,
+      method: 'POST'
+    };
+  } else if (env == "production"){
+    return {
+      env: 'production',
+      dbname: 'stadium_exchange',
+      dbuser: 'stadium_exchange',
+      dbpass: 'game2013',
+      dbhost: 'db.something.com',
+      dbport: 5432,
+      host: 'some.com',
+      port: process.env.PORT,
+      method: 'POST'
+    };
+  }
+}
---
+++ server.js
@@ -0,0 +1,47 @@
+//console.log(process.env);
+//
+
+var express = require('express')
+  , app = express()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server)
+  , http = require('http')
+  , passport = require('passport')
+  , passwordHash = require('password-hash')
+  , flash = require('connect-flash')
+  , LocalStrategy = require('passport-local').Strategy;
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
+app.use(express.logger());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({ secret: "vegetable meat" }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+GAME = {};
+
+GAME.config = require('./config/config.js').config();
+
+// set up models
+GAME.db = require('./models/models.js');
+
+// load auth schemes
+var auth = require('./lib/auth');
+
+// load routes
+var page = require('./routes/page'),
+    util = require('./routes/util');
+
+
+app.get('/', page.index);
+
+// utility routes
+app.get('/healthcheck', util.healthcheck);
+
+
+server.listen(GAME.config.port);
