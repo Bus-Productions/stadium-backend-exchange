98cec691711f8f5d4aa7a1f13dd5718e3680587d
Aziz Shamim <azizshamim@gmail.com>
2013-06-27 15:20:41 -0500

    add script to set up databases for test and dev

--- init_db.sh
+++ init_db.sh
@@ -4,3 +4,8 @@ psql --command "create database stadium_exchange"
 psql --command "create user stadium_exchange with password 'game2013'"
 psql --command "grant all privileges on database stadium_exchange to stadium_exchange"
 psql --command '\list'
+
+psql --command "create database stadium_exchange_test"
+psql --command "create user stadium_exchange_test with password 'game2013'"
+psql --command "grant all privileges on database stadium_exchange to stadium_exchange"
+psql --command '\list'
