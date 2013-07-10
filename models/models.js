51920052f3146e35e677f0eeee47664bb08d4c53
Nathan Loyer <4namlet@gmail.com>
2013-07-10 12:50:53 -0500

    add trade model

--- models/models.js
+++ models/models.js
@@ -10,11 +10,13 @@ var sequelize = new Sequelize(GAME.config.dbname, GAME.config.dbuser, GAME.confi
 var Symbol = require("./symbol")(sequelize, Sequelize);
 var Bid = require("./bid")(sequelize, Sequelize);
 var Ask = require("./ask")(sequelize, Sequelize);
+var Trade = require("./trade")(sequelize, Sequelize);
 var User = require("./user")(sequelize, Sequelize);
 
 exports.Symbol = Symbol;
 exports.Bid = Bid;
 exports.Ask = Ask;
+exports.Trade = Trade;
 exports.User = User;
 
 sequelize.sync().success(function(){
