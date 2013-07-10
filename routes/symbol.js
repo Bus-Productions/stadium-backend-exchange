27b952032c3d04b69732f1d815be6e1eadbfa6c3
Nathan Loyer <4namlet@gmail.com>
2013-07-10 12:40:55 -0500

    create tests for adding and getting symbols

--- routes/symbol.js
+++ routes/symbol.js
@@ -19,3 +19,23 @@ exports.get_symbol = function(req, res){
     res.send(400);
   }
 }
+
+exports.create_symbol = function(req, res){
+  var p = req.body;
+
+  if (p.symbol && p.price && p.issued){
+    GAME.db.Symbol.create({
+      symbol: p.symbol,
+      price: p.price,
+      issued: p.issued
+    }).success(function(symbol){
+      res.send(201, symbol.values);
+    });
+  } else {
+    var emessage = 'You did not send the following parameters: ';
+    if (!p.symbol) emessage += 'symbol ';
+    if (!p.price) emessage += 'price ';
+    if (!p.issued) emessage += 'issued ';
+    res.send(400, emessage);
+  }
+}
