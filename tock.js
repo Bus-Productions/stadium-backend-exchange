b5537d7094cffe86d0e64d259eb0f103b2f4c962
Aziz Shamim <azizshamim@gmail.com>
2013-07-22 15:01:55 -0500

    update tock to fix speed

--- tock.js
+++ tock.js
@@ -1,7 +1,7 @@
 var tick = require('./lib/tick'),
-    config = require('./config/config');
+    config = require('./config/config').config();
 
 var timer = setInterval(function(){
   tick.execute(tick.muckMarket);
-  console.log("TICK");
+  console.log("TICK, next tick in "+config.tick_speed);
 }, config.tick_speed);
